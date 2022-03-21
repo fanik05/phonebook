bf422c5c0cf2fb96c9d2137bbb79890b14df7d8b
fanik05 <fatinanik129@gmail.com>
2022-03-21 17:26:57 +0600

    proxy added

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const base_url = 'http://localhost:3001/persons'
+const base_url = '/api/persons'
 
 const getAll = () => axios.get(base_url).then(response => response.data)
 
